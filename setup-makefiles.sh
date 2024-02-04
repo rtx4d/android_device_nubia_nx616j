fa1e1e473c6d0872321335d2f1c832c7bfb7f849
Andrey Sprynga <rtx4d@aospa.co>
2024-02-04 19:24:34 +0700

    nx616j: Rebrand nx606j tree to nx616j

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2023 The LineageOS Project
+# Copyright (C) 2018-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-export DEVICE=nx606j
+export DEVICE=nx616j
 export DEVICE_COMMON=sdm845-common
 export VENDOR=nubia
 
