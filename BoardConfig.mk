12c19d27ffbc4d2d48930467fa771f3a3a249f36
Andrey Sprynga <rtx4d@aospa.co>
2024-06-04 02:55:08 +0700

    nx616j: sepolicy: Allow read DOUBLE_PROXIMITY sensor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,5 +37,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary version
 include vendor/nubia/nx616j/BoardConfigVendor.mk
